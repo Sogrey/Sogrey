b3aa9295681c334c9b41f1e0c6dc79f6b87d5205
Sogrey <suzhaoheng2006@163.com>
2021-10-07 22:30:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,19 +18,6 @@ const readme = `\
 
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 
-> ⏰ Updated on ${new Date().toUTCString()}
+> ⏰ Updated on ${new Date().toUTCString()}\
 
-## Sogrey
-
-![](https://komarev.com/ghpvc/?username=sogrey&color=dc143c)
-
-一只羊🐑， 两只羊🐑🐑， 三只羊🐑🐑🐑… sorry,GC overhead limit exceeded.
-
-![Sogrey's github stats](https://github-readme-stats.vercel.app/api?username=sogrey&show_icons=true&theme=material-palenight) <!-- https://github.com/anuraghazra/github-readme-stats -->
-
-![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=sogrey&layout=compact)
-
-- 👯 Field of study: Java|Kotlin|Javascript|TypeScript|Flutter|Dart
-- 📫 How to reach me: Xi'an China
-- 😄 Blog: https://sogrey.github.io/ OR https://sogrey.gitee.io/\
 `
