345ae055b1699a402fb424668bb1226e75c98806
Sogrey <suzhaoheng2006@163.com>
2021-10-07 22:21:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,4 +33,4 @@ const readme = `\
 - 👯 Field of study: Java|Kotlin|Javascript|TypeScript|Flutter|Dart
 - 📫 How to reach me: Xi'an China
 - 😄 Blog: https://sogrey.github.io/ OR https://sogrey.gitee.io/
-`
+\`
